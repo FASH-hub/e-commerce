8159d8ba01d71d259f7cd3cefc9ba62c855e60c8
Léon FASHINGABO <53854@etu.he2b.be>
2022-02-08 12:17:06 +0100

    updtes

--- database/migrations/2022_01_12_131716_create_products_table.php
+++ database/migrations/2022_01_12_131716_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
 
             $table->increments('id');
-            $table->string('title');
+            $table->string('name');
             $table->integer('price');
             $table->string('category');
             $table->string('description');
--- database/migrations/2022_01_14_022433_create_carts_table.php
+++ database/migrations/2022_01_14_022433_create_carts_table.php
@@ -15,10 +15,10 @@ class CreateCartsTable extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->integer('product_id')->unsigned();
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('userId')->unsigned();
+            $table->foreign('userId')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('productId')->unsigned();
+            $table->foreign('productId')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
